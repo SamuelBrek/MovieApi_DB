21cf3a8411f2a4d595f39fcd83e553f1cd72e4b9
Samuel Brek Mejia <91771599+SamuelBrek@users.noreply.github.com>
2022-01-26 00:37:34 -0600

    Add files via upload

---
+++ Pelicula.Domain/entities/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Pelicula.Domain.entities
+{
+    public partial class Movie
+    {
+        public int IdMovie { get; set; }
+        public string TitleMovie { get; set; }
+        public string DirectorMovie { get; set; }
+        public string GenreMovie { get; set; }
+        public int? PuntMovie { get; set; }
+        public int? RatingMovie { get; set; }
+        public string YpubliMovie { get; set; }
+    }
+}
